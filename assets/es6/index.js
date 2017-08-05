5ce8ca2a5c1caa06e96545ad2a5bc74a68e6fc3d
Valentin Voilean <valentin@lodgify.com>
2017-08-05 12:45:48 +0200

    ongoing mobile menu dev

--- assets/es6/index.js
+++ assets/es6/index.js
@@ -1,16 +1,16 @@
 import components from './components';
 
-const instances = [];
+window.instances = [];
 
 $(document).ready(() => {
   components.documentReady.forEach(Component => {
-    instances.push(new Component());
+    window.instances.push(new Component());
   });
 });
 
 $(window).on('load', () => {
   components.windowLoad.forEach(Component => {
-    instances.push(new Component());
+    window.instances.push(new Component());
   });
 });
 
