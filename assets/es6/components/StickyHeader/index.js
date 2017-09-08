c263edde1c7b9ab0a5c7b9f13baee0f81ad692fd
Valentin Voilean <valentin@lodgify.com>
2017-09-08 15:17:21 +0200

    updated main menu & sticky header

--- assets/es6/components/StickyHeader/index.js
+++ assets/es6/components/StickyHeader/index.js
@@ -1,25 +1,43 @@
-import inView from 'in-view';
-
+import { throttle } from 'lodash';
 import { CLASS_NAMES } from 'es6/constants';
 import { selectors } from './config';
 
 export default class CurrenciesComponent {
   constructor() {
     this.$header = $(selectors.header);
+    this.$gallery = $(selectors.gallery);
+
+    this._checkHeader = this._checkHeader.bind(this);
+    this._highlightHeader = this._highlightHeader.bind(this);
+    this._resetHeader = this._resetHeader.bind(this);
+
     this.init();
   }
 
   init() {
-    inView.offset(100);
     this._addEventListeners();
+    this._checkHeader();
   }
 
   destroy() {
+    this._removeEventListeners();
+    this._resetHeader();
   }
 
   _addEventListeners() {
-    inView(selectors.gallery).on('exit', $.proxy(this._highlightHeader, this));
-    inView(selectors.gallery).on('enter', $.proxy(this._resetHeader, this));
+    $(window).on('scroll', throttle(this._checkHeader, 100));
+  }
+
+  _removeEventListeners() {
+    $(window).off('scroll', throttle(this._checkHeader, 100));
+  }
+
+  _checkHeader() {
+    if (this.$header.offset().top > this.$gallery.height() - 100) {
+      this._highlightHeader();
+    } else {
+      this._resetHeader();
+    }
   }
 
   _highlightHeader() {
